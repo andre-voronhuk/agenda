8a9c50cc34ea51e8e4148ab2ce85da0c14d0fdc6
andre-voronhuk <45769895+andre-voronhuk@users.noreply.github.com>
2019-03-09 21:57:03 -0300

    Add files via upload

---
+++ ListaTelefonica/ListaTelefonica/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace ListaTelefonica
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ListaTel = new System.Windows.Forms.DataGridView();
+            this.TxtNome = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.TxtTelefone = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.BtnAddTel = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.Nome = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Telefone = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.total = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.ListaTel)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ListaTel
+            // 
+            this.ListaTel.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.ListaTel.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Nome,
+            this.Telefone});
+            this.ListaTel.Location = new System.Drawing.Point(387, 22);
+            this.ListaTel.Name = "ListaTel";
+            this.ListaTel.Size = new System.Drawing.Size(405, 416);
+            this.ListaTel.TabIndex = 0;
+            this.ListaTel.Visible = false;
+            this.ListaTel.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.ListaTel_CellContentClick);
+            // 
+            // TxtNome
+            // 
+            this.TxtNome.Location = new System.Drawing.Point(24, 38);
+            this.TxtNome.Name = "TxtNome";
+            this.TxtNome.Size = new System.Drawing.Size(272, 20);
+            this.TxtNome.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Nome";
+            // 
+            // TxtTelefone
+            // 
+            this.TxtTelefone.Location = new System.Drawing.Point(24, 95);
+            this.TxtTelefone.Name = "TxtTelefone";
+            this.TxtTelefone.Size = new System.Drawing.Size(272, 20);
+            this.TxtTelefone.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(21, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Telefone";
+            // 
+            // BtnAddTel
+            // 
+            this.BtnAddTel.Location = new System.Drawing.Point(221, 149);
+            this.BtnAddTel.Name = "BtnAddTel";
+            this.BtnAddTel.Size = new System.Drawing.Size(75, 23);
+            this.BtnAddTel.TabIndex = 5;
+            this.BtnAddTel.Text = "Cadastrar";
+            this.BtnAddTel.UseVisualStyleBackColor = true;
+            this.BtnAddTel.Click += new System.EventHandler(this.BtnAddTel_Click);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // Nome
+            // 
+            this.Nome.HeaderText = "Nome";
+            this.Nome.Name = "Nome";
+            this.Nome.ReadOnly = true;
+            this.Nome.Width = 200;
+            // 
+            // Telefone
+            // 
+            this.Telefone.HeaderText = "Telefone";
+            this.Telefone.Name = "Telefone";
+            this.Telefone.ReadOnly = true;
+            this.Telefone.Width = 160;
+            // 
+            // total
+            // 
+            this.total.AutoSize = true;
+            this.total.Location = new System.Drawing.Point(327, 38);
+            this.total.Name = "total";
+            this.total.Size = new System.Drawing.Size(42, 13);
+            this.total.TabIndex = 6;
+            this.total.Text = "total: 0 ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.total);
+            this.Controls.Add(this.BtnAddTel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.TxtTelefone);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TxtNome);
+            this.Controls.Add(this.ListaTel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.ListaTel)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView ListaTel;
+        private System.Windows.Forms.TextBox TxtNome;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox TxtTelefone;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button BtnAddTel;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Nome;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Telefone;
+        private System.Windows.Forms.Label total;
+    }
+}
+
---
+++ ListaTelefonica/ListaTelefonica/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ListaTelefonica
+{
+    public partial class Form1 : Form
+    {
+       
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnAddTel_Click(object sender, EventArgs e)
+        {
+            ListaTel.Visible = true;
+            DataGridViewRow row = (DataGridViewRow)ListaTel.Rows[0].Clone();
+
+            row.Cells[0].Value= TxtNome.Text;
+            row.Cells[1].Value = TxtTelefone.Text;
+            ListaTel.Rows.Add(row);
+            int tot = ListaTel.RowCount - 1;
+            total.Text = ("Total: "+tot.ToString());
+            Console.WriteLine(); 
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+
+        }
+
+        private void ListaTel_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            Console.WriteLine(ListaTel);
+
+        }
+    }
+}
